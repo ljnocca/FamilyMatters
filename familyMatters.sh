dcf985ae0de19e343a88f6ee1e24f63d237cf273
Laurent Nocca <ljnocca@gmail.com>
2017-02-02 15:22:52 -0600

    adding familymatters script

---
+++ familyMatters.sh
@@ -0,0 +1,14 @@
+mkdir grandpa
+cd grandpa
+mkdir ma pa
+mkdir ma/baby
+touch README.txt
+curl https://media.giphy.com/media/d341F9XmYTu00/giphy.gif > firstimage.gif
+curl https://media.giphy.com/media/l3q2MyjaBfReViSty/giphy.gif > secondimage.gif
+curl https://media.giphy.com/media/l3q2MyjaBfReViSty/giphy.gif > thirdimage.gif
+echo "John Doe, New York, NY." > john.txt
+echo "Jane Doe, Boston, MA." > jane.txt
+echo "Laurent Nocca, Paris, Fr" > laurent.txt
+mv *.gif pa
+mv *.txt ma
+touch ma/baby/gaga.googoo
